90a14e53f8764a2ab0e786a8f5bbbc6d902f1c02
vineet79ankam <vineet79ankam@gmail.com>
2025-03-31 15:01:23 +0530

    adding script files

---
+++ lead_scoring/01_data_pipeline/scripts/constants.py
@@ -0,0 +1,9 @@
+# You can create more variables according to your project. The following are the basic variables that have been provided to you
+DB_PATH = 
+DB_FILE_NAME = 
+UNIT_TEST_DB_FILE_NAME = 
+DATA_DIRECTORY = 
+INTERACTION_MAPPING = 
+INDEX_COLUMNS = 
+NOT_FEATURES = 
+
