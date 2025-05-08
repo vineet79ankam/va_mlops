02e9fb7f772cf07589c855c6270eb7894f419b4f
vineet79ankam <vineet79ankam@gmail.com>
2025-05-08 10:55:53 +0530

    lead scoring data and training pipeline files updated

---
+++ lead_scoring/_data_pipeline/.ipynb_checkpoints/constants-checkpoint.py
@@ -0,0 +1,18 @@
+DB_PATH = '/home/airflow/dags/Lead_scoring_data_pipeline/'
+DB_FILE_NAME = 'lead_scoring_data_cleaning.db'
+DATA_DIRECTORY = '/home/airflow/dags/Lead_scoring_data_pipeline/data/'
+INTERACTION_MAPPING = '/home/airflow/dags/Lead_scoring_data_pipeline/mapping/interaction_mapping.csv'
+
+
+INDEX_COLUMNS_TRAINING = ['created_date', 'city_tier', 'first_platform_c',
+                'first_utm_medium_c', 'first_utm_source_c', 'total_leads_droppped',
+                'referred_lead', 'app_complete_flag']
+
+INDEX_COLUMNS_INFERENCE = ['created_date', 'city_tier', 'first_platform_c',
+                'first_utm_medium_c', 'first_utm_source_c', 'total_leads_droppped',
+                'referred_lead']
+
+NOT_FEATURES = ['created_date', 'assistance_interaction', 'career_interaction',
+                'payment_interaction', 'social_interaction', 'syllabus_interaction']
+
+
